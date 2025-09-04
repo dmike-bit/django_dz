e910f8b731e02682accf1805828961bcd93c8484
mike <d.mike.s@yandex.ru>
2025-09-04 16:55:25 +0300

    102 Читать README.md

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,77 @@
 from django.contrib import admin
+from .models import Book, Author, Publisher, Genre
 
-# Register your models here.
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description_preview')
+    search_fields = ('name', 'description')
+    list_filter = ('name',)
+    
+    def description_preview(self, obj):
+        return obj.description[:100] + '...' if len(obj.description) > 100 else obj.description
+    description_preview.short_description = 'Описание'
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'birth_date', 'get_age', 'biography_preview')
+    search_fields = ('full_name', 'biography')
+    list_filter = ('birth_date',)
+    date_hierarchy = 'birth_date'
+    
+    def get_age(self, obj):
+        return obj.get_age()
+    get_age.short_description = 'Возраст'
+    
+    def biography_preview(self, obj):
+        return obj.biography[:100] + '...' if len(obj.biography) > 100 else obj.biography
+    biography_preview.short_description = 'Биография'
+
+@admin.register(Publisher)
+class PublisherAdmin(admin.ModelAdmin):
+    list_display = ('name', 'country', 'foundation_year')
+    search_fields = ('name', 'country')
+    list_filter = ('country', 'foundation_year')
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = (
+        'title', 
+        'isbn', 
+        'publication_year', 
+        'publisher', 
+        'get_authors_list', 
+        'get_genres_list',
+        'has_cover'
+    )
+    search_fields = ('title', 'isbn', 'description', 'authors__full_name', 'publisher__name')
+    list_filter = (
+        'publication_year', 
+        'publisher', 
+        'genres',
+        'authors'
+    )
+    filter_horizontal = ('authors', 'genres')
+    readonly_fields = ('cover_preview',)
+    
+    def get_authors_list(self, obj):
+        return obj.get_authors_list()
+    get_authors_list.short_description = 'Авторы'
+    
+    def get_genres_list(self, obj):
+        return obj.get_genres_list()
+    get_genres_list.short_description = 'Жанры'
+    
+    def has_cover(self, obj):
+        return bool(obj.cover)
+    has_cover.boolean = True
+    has_cover.short_description = 'Обложка'
+    
+    def cover_preview(self, obj):
+        if obj.cover:
+            return f'<img src="{obj.cover.url}" style="max-height: 200px;" />'
+        return "Нет обложки"
+    cover_preview.allow_tags = True
+    cover_preview.short_description = 'Превью обложки'
+    
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related('authors', 'genres').select_related('publisher')
--- library/models.py
+++ library/models.py
@@ -1,3 +1,106 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from datetime import date
 
-# Create your models here.
+class Genre(models.Model):
+    """Модель жанра"""
+    name = models.CharField(max_length=100, verbose_name="Название жанра")
+    description = models.TextField(blank=True, verbose_name="Описание")
+    
+    class Meta:
+        verbose_name = "Жанр"
+        verbose_name_plural = "Жанры"
+        ordering = ['name']
+    
+    def __str__(self):
+        return self.name
+
+class Author(models.Model):
+    """Модель автора"""
+    full_name = models.CharField(max_length=200, verbose_name="ФИО автора")
+    birth_date = models.DateField(verbose_name="Дата рождения")
+    biography = models.TextField(blank=True, verbose_name="Биография")
+    
+    class Meta:
+        verbose_name = "Автор"
+        verbose_name_plural = "Авторы"
+        ordering = ['full_name']
+    
+    def __str__(self):
+        return self.full_name
+    
+    def get_age(self):
+        """Возвращает возраст автора"""
+        today = date.today()
+        return today.year - self.birth_date.year - ((today.month, today.day) < (self.birth_date.month, self.birth_date.day))
+
+class Publisher(models.Model):
+    """Модель издательства"""
+    name = models.CharField(max_length=200, verbose_name="Название издательства")
+    country = models.CharField(max_length=100, verbose_name="Страна")
+    foundation_year = models.PositiveIntegerField(
+        verbose_name="Год основания",
+        validators=[
+            MinValueValidator(1000),
+            MaxValueValidator(date.today().year)
+        ]
+    )
+    
+    class Meta:
+        verbose_name = "Издательство"
+        verbose_name_plural = "Издательства"
+        ordering = ['name']
+    
+    def __str__(self):
+        return f"{self.name} ({self.country})"
+
+class Book(models.Model):
+    """Модель книги"""
+    title = models.CharField(max_length=200, verbose_name="Название книги")
+    isbn = models.CharField(max_length=13, unique=True, verbose_name="ISBN")
+    description = models.TextField(blank=True, verbose_name="Описание")
+    publication_year = models.PositiveIntegerField(
+        verbose_name="Год издания",
+        validators=[
+            MinValueValidator(1000),
+            MaxValueValidator(date.today().year)
+        ]
+    )
+    cover = models.ImageField(
+        upload_to='book_covers/',
+        blank=True,
+        null=True,
+        verbose_name="Обложка"
+    )
+    
+    # Связи между моделями
+    authors = models.ManyToManyField(Author, verbose_name="Авторы")
+    publisher = models.ForeignKey(
+        Publisher,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name="Издательство"
+    )
+    genres = models.ManyToManyField(Genre, verbose_name="Жанры")
+    
+    class Meta:
+        verbose_name = "Книга"
+        verbose_name_plural = "Книги"
+        ordering = ['title']
+        indexes = [
+            models.Index(fields=['title']),
+            models.Index(fields=['publication_year']),
+            models.Index(fields=['isbn']),
+        ]
+    
+    def __str__(self):
+        return self.title
+    
+    def get_authors_list(self):
+        """Возвращает список авторов в виде строки"""
+        return ", ".join([author.full_name for author in self.authors.all()])
+    
+    def get_genres_list(self):
+        """Возвращает список жанров в виде строки"""
+        return ", ".join([genre.name for genre in self.genres.all()])
